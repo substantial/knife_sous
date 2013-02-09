e8d4d68cd12798848b499ca24bfc8387cfa43755
Shaun Dern <shaun@substantial.com>
2013-02-08 16:34:18 -0800

    include processor command to sous list

--- lib/chef/knife/sous_list.rb
+++ lib/chef/knife/sous_list.rb
@@ -1,8 +1,11 @@
 require 'chef/knife'
+require 'knife_sous/processor_command'
 
 class Chef
   class Knife
     class SousList < Knife
+      include KnifeSous::ProcessorCommand
+
       banner "knife sous list"
 
       def run
