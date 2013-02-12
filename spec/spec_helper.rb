48f1e86f1ccc49e71eb396da5a825ca36d69eb32
Shaun Dern <shaun@substantial.com>
2013-02-12 11:11:52 -0800

    add shared_example for collection

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,7 @@ RSpec.configure do |config|
 end
 
 require File.join(SPEC_ROOT, '/../lib/knife_sous')
-Dir[File.join(SPEC_ROOT, 'support', '*.rb')].each {|file| require file }
+
+Dir[File.join(SPEC_ROOT, 'support', '*.rb')].each{ |file| require file }
+Dir[File.join(SPEC_ROOT, 'support', 'shared_examples', '*.rb')].each{ |file| require file }
 
