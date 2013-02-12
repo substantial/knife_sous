d293829e40dcff31ebe90a1dbc38f0304522dd4e
Shaun Dern <shaun@substantial.com>
2013-02-12 11:56:08 -0800

    update dsl spec

--- lib/knife_sous/dsl_definitions.rb
+++ lib/knife_sous/dsl_definitions.rb
@@ -4,24 +4,24 @@ module KnifeSous
     def self.included(base)
       base.class_eval do
         base.extend Forwardable
-        def_delegators :@children, :<<, :first, :[], :map, :each
+        def_delegators :children, :<<, :first, :[], :map, :each, :to_a
       end
     end
 
-    def initialize_children
-      @children = []
+    def children
+      @children ||= []
     end
 
     def namespace(name, &block)
       namespace = Namespace.new(name)
       namespace.instance_eval(&block)
-      @children << namespace
+      children << namespace
     end
 
     def node(name, &block)
       node = Node.new(name)
       node.instance_eval(&block)
-      @children << node
+      children << node
     end
   end
 end
--- spec/lib/knife_sous/dsl_definitions_spec.rb
+++ spec/lib/knife_sous/dsl_definitions_spec.rb
@@ -2,54 +2,58 @@ require 'spec_helper'
 
 class DummyClass
   include KnifeSous::DSL
-  def initialize
-    initialize_children
-  end
 end
 
 describe KnifeSous::DSL do
-  let(:dummy_class) { DummyClass.new }
+  it_should_behave_like "a collection object" do
+    let(:collection) { DummyClass.new }
+  end
 
-  describe "Extending class" do
-    it "should add @children to instance_variables" do
-      dummy_class.instance_variables.should include :@children
+  describe "#namespace" do
+    let(:some_block) { Proc.new { 'unicorns' } }
+    let(:dummy_class) { DummyClass.new }
+
+    it "should instantiate a Namespace" do
+      namespace = KnifeSous::Namespace.new('stuff')
+      KnifeSous::Namespace.stub(new: namespace)
+      KnifeSous::Namespace.should_receive(:new).with('some name')
+      dummy_class.namespace('some name', &some_block)
     end
 
-    it "should allow adding and retreiving objects from the class" do
-      dummy_class << 'foo'
-      dummy_class.first.should == 'foo'
+    it "should have the Namespace instance_eval a passed in block" do
+      namespace = KnifeSous::Namespace.new('stuff')
+      KnifeSous::Namespace.stub(new: namespace)
+      namespace.should_receive(:instance_eval).with(&some_block)
+      dummy_class.namespace('some name', &some_block)
     end
 
-    it "should allow direct lookup of element" do
-      dummy_class << 'foo' << 'bar'
-      dummy_class[0].should == 'foo'
-      dummy_class[1].should == 'bar'
+    it "should add the Namespace to the collection" do
+      dummy_class.namespace('some name', &some_block)
+      dummy_class.first.should be_a KnifeSous::Namespace
     end
   end
 
-  [KnifeSous::Node, KnifeSous::Namespace].each do |klass|
-    describe klass do
-      let(:some_block) { Proc.new { 'unicorns' } }
-      let(:method_name) { klass.to_s.split('::').last.downcase }
+  describe "#node" do
+    let(:some_block) { Proc.new { 'unicorns' } }
+    let(:dummy_class) { DummyClass.new }
 
-      it "should instantiate a #{klass}" do
-        # test= KnifeSous::Namespace.new('stuff')
-        # KnifeSous::Namespace.stub(new: namespace)
-        # KnifeSous::Namespace.should_receive(:new).with('some name')
-        # dummy_class.namespace('some name', &some_block)
-      end
+    it "should instantiate a Node" do
+      node = KnifeSous::Node.new('stuff')
+      KnifeSous::Node.stub(new: node)
+      KnifeSous::Node.should_receive(:new).with('some name', &some_block)
+      dummy_class.node('some name', &some_block)
+    end
 
-      it "should have the #{klass} instance_eval a passed in block" do
-        test_klass = klass.new('stuff')
-        klass.stub(new: test_klass)
-        test_klass.should_receive(:instance_eval).with(&some_block)
-        dummy_class.send(method_name,'some name', &some_block)
-      end
+    it "should have the Node instance_eval a passed in block" do
+      node = KnifeSous::Node.new('stuff')
+      KnifeSous::Node.stub(new: node)
+      node.should_receive(:instance_eval).with(&some_block)
+      dummy_class.node('some name', &some_block)
+    end
 
-      it "should add the #{klass} to the children" do
-        dummy_class.send(method_name, 'some name', &some_block)
-        dummy_class.first.should be_a klass
-      end
+    it "should add the Node to the collection" do
+      dummy_class.node('some name', &some_block)
+      dummy_class.first.should be_a KnifeSous::Node
     end
   end
 end
