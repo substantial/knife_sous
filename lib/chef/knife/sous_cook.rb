04256b71310a827f3d34892eb0da64c6637a0dc7
Shaun Dern <shaun@substantial.com>
2013-02-13 15:37:01 -0800

    setup run. Check args and search for node

--- lib/chef/knife/sous_cook.rb
+++ lib/chef/knife/sous_cook.rb
@@ -9,12 +9,29 @@ class Chef
       banner "knife sous cook [NAMESPACE] NODE"
 
       def run
+        check_args
+        search_result = search_for_target
+        cook_target(search_result)
+      end
+
+      def cook_target(target)
+      end
+
+      def search_for_target
+        result = search(name_args)
+        if result.nil?
+          ui.error "Can't find node. Run `knife sous list` to see nodes"
+          exit 1
+        end
+        result
+      end
+
+      def check_args
         unless name_args.size > 0
-          puts "You need to specificy a node or namespace"
+          ui.fatal "You need to specificy a node or namespace"
           show_usage
           exit 1
         end
-        result = search(name_args)
       end
     end
   end
--- spec/lib/chef/knife/sous_cook_spec.rb
+++ spec/lib/chef/knife/sous_cook_spec.rb
@@ -9,6 +9,56 @@ describe Chef::Knife::SousCook do
     end
   end
 
+  describe "#check_args" do
+    it "should print error, show usage and exit if no args are passed in" do
+      cmd = command
+      cmd.ui.should_receive(:fatal).with("You need to specificy a node or namespace")
+      cmd.should_receive(:show_usage)
+      lambda { cmd.check_args}.should raise_error SystemExit
+    end
+  end
+
+  describe "#search_for_target" do
+    it "should search by the args" do
+      search_args = %w[search by these args]
+      cmd = command(*search_args)
+      cmd.stub(search: 'some search result')
+      cmd.should_receive(:search).with(search_args)
+      cmd.search_for_target.should == 'some search result'
+    end
+
+    it "should print error and exit if no results found" do
+      cmd = command
+      cmd.stub(search: nil)
+      cmd.ui.should_receive(:error).with("Can't find node. Run `knife sous list` to see nodes")
+      lambda { cmd.search_for_target }.should raise_error SystemExit
+    end
+  end
+
+  describe "#run" do
+    it "should check that the args exist" do
+      cmd = command
+      cmd.stub(:search_for_target)
+      cmd.should_receive(:check_args)
+      cmd.run
+    end
+
+    it "should search for the target" do
+      cmd = command
+      cmd.stub(:check_args)
+      cmd.should_receive(:search_for_target)
+      cmd.run
+    end
+
+    it "should perform Knife Solo Cook on search result" do
+      cmd = command
+      cmd.stub(:check_args)
+      cmd.stub(search_for_target: "search results")
+      cmd.should_receive(:cook_target).with("search results")
+      cmd.run
+    end
+  end
+
   def command(*args)
     knife_command(Chef::Knife::SousCook, *args)
   end
