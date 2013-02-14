90888b1eef1ca7a4b1d181b212df00dd5b9074c3
Shaun Dern <shaun@substantial.com>
2013-02-13 16:24:22 -0800

    add actual SoloCook support

--- spec/lib/chef/knife/sous_cook_spec.rb
+++ spec/lib/chef/knife/sous_cook_spec.rb
@@ -36,26 +36,77 @@ describe Chef::Knife::SousCook do
   end
 
   describe "#run" do
+    let(:cmd) { command }
+
+    before do
+      cmd.stub(:cook_target)
+    end
+
     it "should check that the args exist" do
-      cmd = command
       cmd.stub(:search_for_target)
       cmd.should_receive(:check_args)
       cmd.run
     end
 
     it "should search for the target" do
-      cmd = command
       cmd.stub(:check_args)
       cmd.should_receive(:search_for_target)
+
       cmd.run
     end
 
     it "should perform Knife Solo Cook on search result" do
-      cmd = command
       cmd.stub(:check_args)
       cmd.stub(search_for_target: "search results")
       cmd.should_receive(:cook_target).with("search results")
       cmd.run
+    end
+  end
+
+  describe "#cook_target" do
+    it "should cook target if its a Node" do
+      node = KnifeSous::Node.new('node')
+      cmd = command
+      cmd.should_receive(:solo_cook_node).with(node)
+      cmd.cook_target(node)
+    end
+
+    it "should cook each child if target is Namespace" do
+      cmd = command
+      namespace = KnifeSous::Namespace.new('namespace')
+      namespace << 'child1' << 'child2'
+      cmd.should_receive(:solo_cook_node).with('child1')
+      cmd.should_receive(:solo_cook_node).with('child2')
+      cmd.cook_target(namespace)
+    end
+  end
+
+  describe "#solo_cook_node" do
+    let(:node) { KnifeSous::Node.new('nodetastic') }
+    let(:cmd) { command }
+    let(:solo_cook_command) { Chef::Knife::SoloCook.new }
+
+    before do
+      Chef::Knife::SoloCook.stub(new: solo_cook_command)
+      solo_cook_command.stub(:run)
+    end
+
+    it "should instantiate a Solo Cook command" do
+      Chef::Knife::SoloCook.should_receive(:new)
+      cmd.solo_cook_node(node)
+    end
+
+    it "should configure the Solo Cook command" do
+      node.stub(ssh_config: 'node ssh_config')
+      node.stub(node_config: 'node config')
+
+      solo_cook_command.should_receive(:run)
+      cmd.solo_cook_node(node)
+
+      solo_cook_command.name_args.should == %w[nodetastic]
+      solo_cook_command.config[:ssh_config].should == 'node ssh_config'
+      solo_cook_command.config[:node_config].should == 'node config'
+      solo_cook_command.config[:chef_node_name].should == 'nodetastic'
     end
   end
 
