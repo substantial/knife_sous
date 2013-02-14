2d7c4194e1abf644a926afdf72b7dce4fdaeadb8
Shaun Dern <shaun@substantial.com>
2013-02-14 14:16:12 -0800

    update configure_command

--- lib/knife_sous/configure_command.rb
+++ lib/knife_sous/configure_command.rb
@@ -2,15 +2,7 @@ module KnifeSous
   module ConfigureCommand
 
     def configure_command(command, node)
-      node_methods = [:ssh_config_file, :node_config, :ssh_port,
-                      :identity_file, :ssh_user, :ssh_password]
-      node_config = {}
-
-      node_methods.each do |method|
-        node_config[method] = node.send(method)
-      end
-
-      command.config.merge!(node_config)
+      command.config.merge!(node.config)
       command
     end
   end
--- spec/lib/knife_sous/configure_command_spec.rb
+++ spec/lib/knife_sous/configure_command_spec.rb
@@ -9,30 +9,27 @@ describe KnifeSous::ConfigureCommand do
 
 
   describe "#configure_command" do
-    let(:node) { KnifeSous::Node.new('some node') }
 
     describe "#node_name" do
       it "should set the node name" do
-        node.evaluate_block do
-          node_name :node_tastic
-        end
+        node = KnifeSous::Node.new('some node', node_name: 'node_tastic')
         node.node_name.should == 'node_tastic'
       end
 
       it "should default to the node name" do
+        node = KnifeSous::Node.new('some node')
         node.node_name.should == 'some node'
       end
     end
 
     it "should be configured for a given node" do
-      node.evaluate_block do |n|
-        n.ssh_config_file 'ssh config'
-        n.node_config 'node config'
-        n.ssh_user 'some_user'
-        n.ssh_port 22
-        n.identity_file 'path/to/iden/file'
-        n.ssh_password 'some_password'
-      end
+      node = KnifeSous::Node.new('some node',
+                                 ssh_config_file: 'ssh config',
+                                 node_config: 'node config',
+                                 ssh_user: 'some_user',
+                                 ssh_port: 22,
+                                 identity_file: '/path/to/iden/file',
+                                 ssh_password: 'some_password')
 
       other_command = command
       command.configure_command(other_command, node)
@@ -40,7 +37,7 @@ describe KnifeSous::ConfigureCommand do
       other_command.config[:ssh_config_file].should == 'ssh config'
       other_command.config[:node_config].should == 'node config'
       other_command.config[:ssh_port].should == '22'
-      other_command.config[:identity_file].should == 'path/to/iden/file'
+      other_command.config[:identity_file].should == '/path/to/iden/file'
       other_command.config[:ssh_user].should == 'some_user'
       other_command.config[:ssh_password].should == 'some_password'
     end
