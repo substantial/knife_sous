168574a30965f1a455a2da05c6f8d92119aa23ea
Shaun Dern <shaun@substantial.com>
2013-02-14 20:12:41 -0800

    use node command shared example for cook

--- lib/chef/knife/sous_cook.rb
+++ lib/chef/knife/sous_cook.rb
@@ -1,12 +1,14 @@
 require 'chef/knife'
 require 'knife_sous/processor_command'
 require 'knife_sous/configure_command'
+require 'knife_sous/node_command'
 
 class Chef
   class Knife
     class SousCook < Knife
       include KnifeSous::ProcessorCommand
       include KnifeSous::ConfigureCommand
+      include KnifeSous::NodeCommand
 
       deps do
         require 'chef/knife/solo_cook'
@@ -36,23 +38,6 @@ class Chef
         solo_cook_command.name_args << node.hostname
         solo_cook_command.name_args << node.node_config
         solo_cook_command.run
-      end
-
-      def search_for_target
-        result = search(name_args)
-        if result.nil?
-          ui.error "Can't find node. Run `knife sous list` to see nodes"
-          exit 1
-        end
-        result
-      end
-
-      def check_args
-        unless name_args.size > 0
-          ui.fatal "You need to specificy a node or namespace"
-          show_usage
-          exit 1
-        end
       end
     end
   end
--- spec/lib/chef/knife/sous_cook_spec.rb
+++ spec/lib/chef/knife/sous_cook_spec.rb
@@ -9,30 +9,8 @@ describe Chef::Knife::SousCook do
     end
   end
 
-  describe "#check_args" do
-    it "should print error, show usage and exit if no args are passed in" do
-      cmd = command
-      cmd.ui.should_receive(:fatal).with("You need to specificy a node or namespace")
-      cmd.should_receive(:show_usage)
-      lambda { cmd.check_args}.should raise_error SystemExit
-    end
-  end
-
-  describe "#search_for_target" do
-    it "should search by the args" do
-      search_args = %w[search by these args]
-      cmd = command(*search_args)
-      cmd.stub(search: 'some search result')
-      cmd.should_receive(:search).with(search_args)
-      cmd.search_for_target.should == 'some search result'
-    end
-
-    it "should print error and exit if no results found" do
-      cmd = command
-      cmd.stub(search: nil)
-      cmd.ui.should_receive(:error).with("Can't find node. Run `knife sous list` to see nodes")
-      lambda { cmd.search_for_target }.should raise_error SystemExit
-    end
+  it_should_behave_like "a node command" do
+    let(:node_command) { command }
   end
 
   describe "#run" do
